8ec0b6881e3a41b875ed8c52bba27ac382accdaf
rosskaplan <ross.kaplan96@gmail.com>
2017-05-03 01:16:38 -0400

    sender all setup for basic sending of single packets with no error

--- sender.py
+++ sender.py
@@ -1,20 +1,43 @@
 import pleasetransfer
 import socket
+import sys
+import hashlib
+from hashlib import md5
+import binascii
 
 t=pleasetransfer.pleasetransfer(True)#true for sender
 t.send_setup()
 t.rec_setup()
 t.settimeout(1)
+all_data = "".join(sys.stdin)
+x = 1006; #1006 bytes per segment
+m = hashlib.md5();
+new_data = [];
+full_data = [];
+check = [];
+output = "";
+for i in range(0, (len(all_data)//x)+1):
+    output = "";
+    new_data.append(all_data[i:i+x]);
+    m.update(new_data[i]);
+    check.append(m.digest());
+    output = str(int(bin(i%65536)[2:],base=2));
+    output += check[i];
+    output += new_data[i];
+    full_data.append(output);
+
 while True:
     try:
-        t.sendbits(bin(2344))
-        ack=t.recbits()
-        print ack
+        for i in range(0, (len(all_data)//x)+1):
+            t.sendbits(bin(full_data[i]));
+            ack=t.recbits()
+            print ack
         break
     except socket.timeout:
         pass
 
 # 1. create file
+# 2 byte number, 16 byte checksum, 1006 byte other
 # 2. write a header function that takes in bits, computes the checksum, and packet number
 # 3. append header with bits of length of your choice
 # 4. send them, receive the bits
