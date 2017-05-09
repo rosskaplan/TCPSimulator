8e07e594f95568a105752ef3613cb990720294f4
Abhinav Jain <jain@cooper.edu>
2017-05-08 22:26:08 -0400

    updated receiver with pseudocode

--- rec.py
+++ rec.py
@@ -1,8 +1,30 @@
 import pleasetransfer
+import hashlib
+from hashlib import md5
+import binascii
+import sys
+
 t=pleasetransfer.pleasetransfer(False)#False for receiver
 t.rec_setup()
 t.send_setup()
 # t.settimeout(10)
+
+all_data = "".join(sys.stdout);
+x = 1006;
+m = hashlib.md5()
+new_data = []
+full_data = []
+check = "";
+num = "";
+output = ""
+i = 0;
 while True:
-    print t.recbits()
-    t.sendbits(bin(123))
+	output = "";):
+	new_data = str(t.recbits())
+	for j in range (0, 1)
+		num.append(new_data[j]
+	num = int(num);
+	if (num == i)
+		for j in range (2, 18)
+			check.append
+		t.sendbits(bin(i))
