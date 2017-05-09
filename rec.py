579fb49a38862411655381424898dfaa2b48a27a
rosskaplan <ross.kaplan96@gmail.com>
2017-05-09 02:27:00 -0400

    fixed your dumb editor's stuff

--- rec.py
+++ rec.py
@@ -24,11 +24,10 @@ while True:
     new_data = t.recbits();
     #print new_data[2:10];
     num = int(new_data[2:10], base = 2);
-    print num;
     if (num == i):
-		print(num);
+        print(num);
         output = new_data[6:];
-		check = new_data[2:4];
+        check = new_data[2:4];
         new_check = zlib.adler32(output);
         if (new_check == check):
             #print(output);
