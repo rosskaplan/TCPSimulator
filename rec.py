9663599814cec4daedda2e03f28bca837f967aee
Abhinav Jain <jain@cooper.edu>
2017-05-09 02:25:32 -0400

    blah

--- rec.py
+++ rec.py
@@ -22,14 +22,13 @@ retval = "";
 while True:
     retval = "";
     new_data = t.recbits();
-    print new_data[2:10];
-    num = int(new_data[0]);
-    #print num;
+    #print new_data[2:10];
+    num = int(new_data[2:10], base = 2);
+    print num;
     if (num == i):
+		print(num);
         output = new_data[6:];
-        #print(output);
-        check = new_data[2:4];
-        #print check;
+		check = new_data[2:4];
         new_check = zlib.adler32(output);
         if (new_check == check):
             #print(output);
